c6e0717c501eb87c2e9314db33b14e395eb4888c
nnannaegbujiobi <nnanna@newvybe.com>
2019-03-08 19:28:58 -0600

    api_key

---
+++ src/lib/API.js
@@ -0,0 +1,22 @@
+// const API_URL = 'http://api.openweathermap.org/data/2.5/weather?q=madison,us&APPID=d41563fcfae1b65a7d33342483a4157a';
+
+const API_KEY = 'd41563fcfae1b65a7d33342483a4157a';
+
+const location = 'madison,us';
+
+const API_URL = `http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=${API_KEY}`;
+
+
+function getWeather() {
+  fetch(API_URL)
+    .then(response => response.json())
+    .then(result => { 
+      console.log(result); 
+    });
+}
+
+
+export default {
+  getWeather
+
+};
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import API from '@/lib/API';
 
 export default {
   name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+  mounted() {
+    API.getWeather();
+  },
+  
+};
 </script>
