7727adc44c6e53678e4683678a90268a8cb64dc7
nnannaegbujiobi <nnanna@newvybe.com>
2019-03-09 17:01:18 -0600

    all cities/temp

--- src/lib/API.js
+++ src/lib/API.js
@@ -1,22 +1,22 @@
 // const API_URL = 'http://api.openweathermap.org/data/2.5/weather?q=madison,us&APPID=d41563fcfae1b65a7d33342483a4157a';
 
-const API_KEY = 'd41563fcfae1b65a7d33342483a4157a';
+// const API_KEY = 'd41563fcfae1b65a7d33342483a4157a';
 
-const location = 'madison,us';
+// const location = 'madison,us';
 
-const API_URL = `http://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=${API_KEY}`;
-
-
-function getWeather() {
-  return fetch(API_URL)
-    .then(response => response.json())
-    .then(result => { 
-      // console.log(result); 
-    });
-}
+// const API_URL = `http://api.openweathermap.org/data/2.5/group?id=4684888,4887398,5263045,5037649&APPID=d41563fcfae1b65a7d33342483a4157a&units=imperial`;
 
 
-export default {
-  getWeather
+// function getWeather() {
+//   return fetch(API_URL)
+//     .then(response => response.json())
+//     .then(result => { 
+//       // console.log(result); 
+//     });
+// }
 
-};
+
+// export default {
+//   getWeather
+
+// };
